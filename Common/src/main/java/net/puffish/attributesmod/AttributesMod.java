c0bc02b5134c7731ef53141037cbbfef74ae08dd
Pufferfish <puffish@puffish.net>
2024-03-13 10:22:46 +0100

    Split from Pufferfish's Skills

---
+++ Common/src/main/java/net/puffish/attributesmod/AttributesMod.java
@@ -0,0 +1,208 @@
+package net.puffish.attributesmod;
+
+import net.minecraft.entity.attribute.ClampedEntityAttribute;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+import net.puffish.attributesmod.attribute.DynamicEntityAttribute;
+import net.puffish.attributesmod.mixin.EntityAttributeInstanceInvoker;
+import net.puffish.attributesmod.util.Registrar;
+import net.puffish.attributesmod.util.Signed;
+
+public class AttributesMod {
+	public static final String MOD_ID = "puffish_attributes";
+
+	public static final Identifier STAMINA_ID
+			= AttributesMod.createAttributeIdentifier("player", "stamina");
+	public static final EntityAttribute STAMINA = createClampedAttribute(
+			STAMINA_ID,
+			4.0,
+			0.0,
+			1024.0
+	).setTracked(true);
+
+	public static final Identifier MELEE_DAMAGE_ID
+			= AttributesMod.createAttributeIdentifier("player", "melee_damage");
+	public static final EntityAttribute MELEE_DAMAGE = createDynamicAttribute(
+			MELEE_DAMAGE_ID,
+			0.0,
+			Double.MAX_VALUE
+	);
+
+	public static final Identifier RANGED_DAMAGE_ID
+			= AttributesMod.createAttributeIdentifier("player", "ranged_damage");
+	public static final EntityAttribute RANGED_DAMAGE = createDynamicAttribute(
+			RANGED_DAMAGE_ID,
+			0.0,
+			Double.MAX_VALUE
+	);
+
+	public static final Identifier FORTUNE_ID
+			= AttributesMod.createAttributeIdentifier("player", "fortune");
+	public static final EntityAttribute FORTUNE = createDynamicAttribute(
+			FORTUNE_ID,
+			0.0,
+			Double.MAX_VALUE
+	);
+
+	public static final Identifier HEALING_ID
+			= AttributesMod.createAttributeIdentifier("player", "healing");
+	public static final EntityAttribute HEALING = createDynamicAttribute(
+			HEALING_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier JUMP_ID
+			= AttributesMod.createAttributeIdentifier("player", "jump");
+	public static final EntityAttribute JUMP = createDynamicAttribute(
+			JUMP_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier RESISTANCE_ID
+			= AttributesMod.createAttributeIdentifier("player", "resistance");
+	public static final EntityAttribute RESISTANCE = createDynamicAttribute(
+			RESISTANCE_ID,
+			0.0,
+			Double.MAX_VALUE
+	);
+
+	public static final Identifier MINING_SPEED_ID
+			= AttributesMod.createAttributeIdentifier("player", "mining_speed");
+	public static final EntityAttribute MINING_SPEED = createDynamicAttribute(
+			MINING_SPEED_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier PICKAXE_SPEED_ID
+			= AttributesMod.createAttributeIdentifier("player", "pickaxe_speed");
+	public static final EntityAttribute PICKAXE_SPEED = createDynamicAttribute(
+			PICKAXE_SPEED_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier AXE_SPEED_ID
+			= AttributesMod.createAttributeIdentifier("player", "axe_speed");
+	public static final EntityAttribute AXE_SPEED = createDynamicAttribute(
+			AXE_SPEED_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier SHOVEL_SPEED_ID
+			= AttributesMod.createAttributeIdentifier("player", "shovel_speed");
+	public static final EntityAttribute SHOVEL_SPEED = createDynamicAttribute(
+			SHOVEL_SPEED_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier SPRINTING_SPEED_ID
+			= AttributesMod.createAttributeIdentifier("player", "sprinting_speed");
+	public static final EntityAttribute SPRINTING_SPEED = createDynamicAttribute(
+			SPRINTING_SPEED_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static final Identifier KNOCKBACK_ID
+			= AttributesMod.createAttributeIdentifier("player", "knockback");
+	public static final EntityAttribute KNOCKBACK = createDynamicAttribute(
+			KNOCKBACK_ID,
+			0.0,
+			Double.MAX_VALUE
+	).setTracked(true);
+
+	public static void setup(Registrar registrar) {
+		registrar.register(Registries.ATTRIBUTE, STAMINA_ID, STAMINA);
+		registrar.register(Registries.ATTRIBUTE, MELEE_DAMAGE_ID, MELEE_DAMAGE);
+		registrar.register(Registries.ATTRIBUTE, RANGED_DAMAGE_ID, RANGED_DAMAGE);
+		registrar.register(Registries.ATTRIBUTE, FORTUNE_ID, FORTUNE);
+		registrar.register(Registries.ATTRIBUTE, HEALING_ID, HEALING);
+		registrar.register(Registries.ATTRIBUTE, JUMP_ID, JUMP);
+		registrar.register(Registries.ATTRIBUTE, RESISTANCE_ID, RESISTANCE);
+		registrar.register(Registries.ATTRIBUTE, MINING_SPEED_ID, MINING_SPEED);
+		registrar.register(Registries.ATTRIBUTE, PICKAXE_SPEED_ID, PICKAXE_SPEED);
+		registrar.register(Registries.ATTRIBUTE, AXE_SPEED_ID, AXE_SPEED);
+		registrar.register(Registries.ATTRIBUTE, SHOVEL_SPEED_ID, SHOVEL_SPEED);
+		registrar.register(Registries.ATTRIBUTE, SPRINTING_SPEED_ID, SPRINTING_SPEED);
+		registrar.register(Registries.ATTRIBUTE, KNOCKBACK_ID, KNOCKBACK);
+	}
+
+	public static Identifier createIdentifier(String path) {
+		return new Identifier(MOD_ID, path);
+	}
+
+	public static Identifier createAttributeIdentifier(String type, String name) {
+		return createIdentifier(type + "." + name);
+	}
+
+	public static EntityAttribute createClampedAttribute(Identifier id, double fallback, double min, double max) {
+		return new ClampedEntityAttribute(
+				id.toTranslationKey("attribute"),
+				fallback,
+				min,
+				max
+		);
+	}
+
+	public static EntityAttribute createDynamicAttribute(Identifier id, double min, double max) {
+		return new DynamicEntityAttribute(
+				id.toTranslationKey("attribute"),
+				min,
+				max
+		);
+	}
+
+	@SafeVarargs
+	public static double applyAttributeModifiers(
+			double initial,
+			Signed<EntityAttributeInstance>... attributes
+	) {
+		for (var signedAttribute : attributes) {
+			for (var modifier : ((EntityAttributeInstanceInvoker) signedAttribute.value())
+					.invokeGetModifiersByOperation(EntityAttributeModifier.Operation.ADDITION)
+			) {
+				switch (signedAttribute.sign()) {
+					case POSITIVE -> initial += modifier.getValue();
+					case NEGATIVE -> initial -= modifier.getValue();
+					default -> throw new IllegalStateException();
+				}
+			}
+		}
+		double result = initial;
+		for (var signedAttribute : attributes) {
+			for (var modifier : ((EntityAttributeInstanceInvoker) signedAttribute.value())
+					.invokeGetModifiersByOperation(EntityAttributeModifier.Operation.MULTIPLY_BASE)
+			) {
+				switch (signedAttribute.sign()) {
+					case POSITIVE -> result += initial * modifier.getValue();
+					case NEGATIVE -> result -= initial * modifier.getValue();
+					default -> throw new IllegalStateException();
+				}
+			}
+		}
+		for (var signedAttribute : attributes) {
+			for (var modifier : ((EntityAttributeInstanceInvoker) signedAttribute.value())
+					.invokeGetModifiersByOperation(EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+			) {
+				switch (signedAttribute.sign()) {
+					case POSITIVE -> result *= 1.0 + modifier.getValue();
+					case NEGATIVE -> result *= 1.0 - modifier.getValue();
+					default -> throw new IllegalStateException();
+				}
+			}
+		}
+		for (var signedAttribute : attributes) {
+			result = signedAttribute.value().getAttribute().clamp(result);
+		}
+		return result;
+	}
+
+}
---
+++ Common/src/main/java/net/puffish/attributesmod/mixin/PlayerEntityMixin.java
@@ -0,0 +1,76 @@
+package net.puffish.attributesmod.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.puffish.attributesmod.AttributesMod;
+import net.puffish.attributesmod.util.Sign;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin {
+
+	private static final double VANILLA_KNOCKBACK = 0.4;
+
+	@Inject(method = "createPlayerAttributes", at = @At("RETURN"))
+	private static void injectAtCreatePlayerAttributes(
+			CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir
+	) {
+		cir.getReturnValue()
+				.add(AttributesMod.STAMINA)
+				.add(AttributesMod.FORTUNE)
+				.add(AttributesMod.RANGED_DAMAGE)
+				.add(AttributesMod.MELEE_DAMAGE)
+				.add(AttributesMod.HEALING)
+				.add(AttributesMod.JUMP)
+				.add(AttributesMod.RESISTANCE)
+				.add(AttributesMod.MINING_SPEED)
+				.add(AttributesMod.SPRINTING_SPEED)
+				.add(AttributesMod.KNOCKBACK);
+	}
+
+	@Inject(
+			method = "attack",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/enchantment/EnchantmentHelper;onTargetDamaged(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/Entity;)V"
+			)
+	)
+	private void injectAtAttack(Entity target, CallbackInfo ci) {
+		var player = (PlayerEntity) (Object) this;
+
+		var knockback = AttributesMod.applyAttributeModifiers(
+				VANILLA_KNOCKBACK,
+				Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.KNOCKBACK))
+		) - VANILLA_KNOCKBACK;
+
+		var yaw = player.getYaw() * MathHelper.RADIANS_PER_DEGREE;
+		var sin = MathHelper.sin(yaw);
+		var cos = MathHelper.cos(yaw);
+
+		if (target instanceof LivingEntity livingEntity) {
+			livingEntity.takeKnockback(knockback, sin, -cos);
+		} else {
+			target.addVelocity(-sin * knockback, 0, cos * knockback);
+		}
+	}
+
+	@Inject(method = "getMovementSpeed()F", at = @At("RETURN"), cancellable = true)
+	private void injectAtGetMovementSpeed(CallbackInfoReturnable<Float> cir) {
+		var player = (PlayerEntity) (Object) this;
+
+		if (player.isSprinting()) {
+			cir.setReturnValue((float) AttributesMod.applyAttributeModifiers(
+					cir.getReturnValueF(),
+					Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.SPRINTING_SPEED))
+			));
+		}
+	}
+
+}
