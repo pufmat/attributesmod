f0af9315d15ad006af35eda1977c0309c8c76b98
Pufferfish <puffish@puffish.net>
2024-03-13 10:22:46 +0100

    Split from Pufferfish's Skills

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        maven(url = "https://maven.fabricmc.net/")
+        maven(url = "https://maven.neoforged.net/releases/")
+        maven(url = "https://maven.architectury.dev/")
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.architectury.loom") version "1.5.388" apply false
+}
+
+rootProject.name = "Pufferfish's Attributes"
+
+include("Common", "Fabric", "NeoForge")
