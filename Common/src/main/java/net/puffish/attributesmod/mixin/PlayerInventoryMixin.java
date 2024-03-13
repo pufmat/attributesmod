231f1592c6e54d95ca9634d14dd79809b8ab08fe
Pufferfish <puffish@puffish.net>
2024-03-13 10:22:46 +0100

    Add attributes for tools

--- Common/src/main/java/net/puffish/attributesmod/mixin/PlayerInventoryMixin.java
+++ Common/src/main/java/net/puffish/attributesmod/mixin/PlayerInventoryMixin.java
@@ -18,15 +18,31 @@ import java.util.ArrayList;
 @Mixin(PlayerInventory.class)
 public class PlayerInventoryMixin {
 
+	@SuppressWarnings("unchecked")
 	@Inject(method = "getBlockBreakingSpeed", at = @At("RETURN"), cancellable = true)
 	private void injectAtGetBlockBreakingSpeed(CallbackInfoReturnable<Float> cir) {
 		if (cir.getReturnValueF() > 1.0f) { // This check is required to not break vanilla enchantments behavior
 			var inventory = ((PlayerInventory) (Object) this);
 			var player = inventory.player;
+			var item = inventory.getMainHandStack().getItem();
+
+			var attributes = new ArrayList<Signed<EntityAttributeInstance>>();
+
+			if (item instanceof PickaxeItem) {
+				attributes.add(Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.PICKAXE_SPEED)));
+			}
+			if (item instanceof AxeItem) {
+				attributes.add(Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.AXE_SPEED)));
+			}
+			if (item instanceof ShovelItem) {
+				attributes.add(Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.SHOVEL_SPEED)));
+			}
+
+			attributes.add(Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.MINING_SPEED)));
 
 			cir.setReturnValue((float) AttributesMod.applyAttributeModifiers(
 					cir.getReturnValueF(),
-					Sign.POSITIVE.wrap(player.getAttributeInstance(AttributesMod.MINING_SPEED))
+					attributes.toArray(Signed[]::new)
 			));
 		}
 	}
