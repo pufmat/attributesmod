a6f82db888424b0e5f3004649bbd36da66673bbe
Pufferfish <puffish@puffish.net>
2024-07-15 12:54:57 +0200

    Add compatibility with AttributesLib

--- Common/src/main/java/net/puffish/attributesmod/AttributesMod.java
+++ Common/src/main/java/net/puffish/attributesmod/AttributesMod.java
@@ -31,132 +31,92 @@ public class AttributesMod {
 			= AttributesMod.createAttributeIdentifier("player", "melee_damage");
 	public static final RegistryEntry<EntityAttribute> MELEE_DAMAGE = registerAttribute(
 			MELEE_DAMAGE_ID,
-			createDynamicAttribute(
-					MELEE_DAMAGE_ID,
-					0.0,
-					Double.MAX_VALUE
-			)
+			createDynamicAttribute(MELEE_DAMAGE_ID)
 	);
 
 	public static final Identifier RANGED_DAMAGE_ID
 			= AttributesMod.createAttributeIdentifier("player", "ranged_damage");
 	public static final RegistryEntry<EntityAttribute> RANGED_DAMAGE = registerAttribute(
 			RANGED_DAMAGE_ID,
-			createDynamicAttribute(
-					RANGED_DAMAGE_ID,
-					0.0,
-					Double.MAX_VALUE
-			));
+			createDynamicAttribute(RANGED_DAMAGE_ID)
+	);
 
 	public static final Identifier FORTUNE_ID
 			= AttributesMod.createAttributeIdentifier("player", "fortune");
 	public static final RegistryEntry<EntityAttribute> FORTUNE = registerAttribute(
 			FORTUNE_ID,
-			createDynamicAttribute(
-					FORTUNE_ID,
-					0.0,
-					Double.MAX_VALUE
-			));
+			createDynamicAttribute(FORTUNE_ID)
+	);
 
 	public static final Identifier HEALING_ID
 			= AttributesMod.createAttributeIdentifier("player", "healing");
 	public static final RegistryEntry<EntityAttribute> HEALING = registerAttribute(
 			HEALING_ID,
-			createDynamicAttribute(
-					HEALING_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(HEALING_ID).setTracked(true)
+	);
 
 	public static final Identifier JUMP_ID
 			= AttributesMod.createAttributeIdentifier("player", "jump");
 	public static final RegistryEntry<EntityAttribute> JUMP = registerAttribute(
 			JUMP_ID,
-			createDynamicAttribute(
-					JUMP_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(JUMP_ID).setTracked(true)
+	);
 
 	public static final Identifier RESISTANCE_ID
 			= AttributesMod.createAttributeIdentifier("player", "resistance");
 	public static final RegistryEntry<EntityAttribute> RESISTANCE = registerAttribute(
 			RESISTANCE_ID,
-			createDynamicAttribute(
-					RESISTANCE_ID,
-					0.0,
-					Double.MAX_VALUE
-			));
+			createDynamicAttribute(RESISTANCE_ID)
+	);
 
 	public static final Identifier MINING_SPEED_ID
 			= AttributesMod.createAttributeIdentifier("player", "mining_speed");
 	public static final RegistryEntry<EntityAttribute> MINING_SPEED = registerAttribute(
 			MINING_SPEED_ID,
-			createDynamicAttribute(
-					MINING_SPEED_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(MINING_SPEED_ID).setTracked(true)
+	);
 
 	public static final Identifier PICKAXE_SPEED_ID
 			= AttributesMod.createAttributeIdentifier("player", "pickaxe_speed");
 	public static final RegistryEntry<EntityAttribute> PICKAXE_SPEED = registerAttribute(
 			PICKAXE_SPEED_ID,
-			createDynamicAttribute(
-					PICKAXE_SPEED_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(PICKAXE_SPEED_ID).setTracked(true)
+	);
 
 	public static final Identifier AXE_SPEED_ID
 			= AttributesMod.createAttributeIdentifier("player", "axe_speed");
 	public static final RegistryEntry<EntityAttribute> AXE_SPEED = registerAttribute(
 			AXE_SPEED_ID,
-			createDynamicAttribute(
-					AXE_SPEED_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(AXE_SPEED_ID).setTracked(true)
+	);
 
 	public static final Identifier SHOVEL_SPEED_ID
 			= AttributesMod.createAttributeIdentifier("player", "shovel_speed");
 	public static final RegistryEntry<EntityAttribute> SHOVEL_SPEED = registerAttribute(
 			SHOVEL_SPEED_ID,
-			createDynamicAttribute(
-					SHOVEL_SPEED_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(SHOVEL_SPEED_ID).setTracked(true)
+	);
 
 	public static final Identifier SPRINTING_SPEED_ID
 			= AttributesMod.createAttributeIdentifier("player", "sprinting_speed");
 	public static final RegistryEntry<EntityAttribute> SPRINTING_SPEED = registerAttribute(
 			SPRINTING_SPEED_ID,
-			createDynamicAttribute(
-					SPRINTING_SPEED_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(SPRINTING_SPEED_ID).setTracked(true)
+	);
 
 	public static final Identifier KNOCKBACK_ID
 			= AttributesMod.createAttributeIdentifier("player", "knockback");
 	public static final RegistryEntry<EntityAttribute> KNOCKBACK = registerAttribute(
 			KNOCKBACK_ID,
-			createDynamicAttribute(
-					KNOCKBACK_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(KNOCKBACK_ID).setTracked(true)
+	);
 
 	public static final Identifier REPAIR_COST_ID
 			= AttributesMod.createAttributeIdentifier("player", "repair_cost");
 	public static final RegistryEntry<EntityAttribute> REPAIR_COST = registerAttribute(
 			REPAIR_COST_ID,
-			createDynamicAttribute(
-					REPAIR_COST_ID,
-					0.0,
-					Double.MAX_VALUE
-			).setTracked(true));
+			createDynamicAttribute(REPAIR_COST_ID).setTracked(true)
+	);
 
 	public static void setup() {
 
@@ -179,11 +139,9 @@ public class AttributesMod {
 		);
 	}
 
-	public static EntityAttribute createDynamicAttribute(Identifier id, double min, double max) {
+	public static EntityAttribute createDynamicAttribute(Identifier id) {
 		return new DynamicEntityAttribute(
-				id.toTranslationKey("attribute"),
-				min,
-				max
+				id.toTranslationKey("attribute")
 		);
 	}
 
