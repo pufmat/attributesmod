1dedb04dbafd93ecfbc52f4d340db515611d2e87
Pufferfish <puffish@puffish.net>
2025-12-01 21:24:31 +0100

    Add code generator for attributes

--- Common/src/main/java/net/puffish/attributesmod/mixin/ExperienceOrbEntityMixin.java
+++ Common/src/main/java/net/puffish/attributesmod/mixin/ExperienceOrbEntityMixin.java
@@ -3,8 +3,8 @@ package net.puffish.attributesmod.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.puffish.attributesmod.AttributesMod;
 import net.puffish.attributesmod.api.DynamicModification;
+import net.puffish.attributesmod.api.PuffishAttributes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -22,7 +22,7 @@ public class ExperienceOrbEntityMixin {
 	)
 	private int modifyArgAtAddExperience(int experience, @Local(argsOnly = true) PlayerEntity player) {
 		return Math.round(DynamicModification.create()
-				.withPositive(AttributesMod.EXPERIENCE, player)
+				.withPositive(PuffishAttributes.EXPERIENCE, player)
 				.applyTo(experience));
 	}
 
