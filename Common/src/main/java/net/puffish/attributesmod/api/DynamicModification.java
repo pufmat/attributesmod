c55eb954d710db2f0c6cc6739e511871fa98e25b
Pufferfish <puffish@puffish.net>
2026-01-29 15:24:30 +0100

    Add consuming speed attribute

--- Common/src/main/java/net/puffish/attributesmod/api/DynamicModification.java
+++ Common/src/main/java/net/puffish/attributesmod/api/DynamicModification.java
@@ -16,6 +16,9 @@ public interface DynamicModification {
 	double applyTo(double value);
 	float applyTo(float value);
 
+	double applyToReciprocal(double value);
+	float applyToReciprocal(float value);
+
 	double relativeTo(double value);
 	float relativeTo(float value);
 }
