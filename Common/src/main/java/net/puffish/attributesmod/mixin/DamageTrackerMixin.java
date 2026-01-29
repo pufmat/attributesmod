e39635868a43991fd229d755febcd91a719a0388
Pufferfish <puffish@puffish.net>
2026-01-29 16:56:59 +0100

    Add damage reflection attribute

--- Common/src/main/java/net/puffish/attributesmod/mixin/DamageTrackerMixin.java
+++ Common/src/main/java/net/puffish/attributesmod/mixin/DamageTrackerMixin.java
@@ -3,15 +3,22 @@ package net.puffish.attributesmod.mixin;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTracker;
+import net.minecraft.entity.damage.EntityDamageSource;
 import net.puffish.attributesmod.api.DynamicModification;
 import net.puffish.attributesmod.api.PuffishAttributes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DamageTracker.class)
 public class DamageTrackerMixin {
+
+	@Shadow
+	@Final
+	private LivingEntity entity;
 
 	@Inject(
 			method = "onDamage",
@@ -25,6 +32,16 @@ public class DamageTrackerMixin {
 
 			if (lifeSteal > 0) {
 				attacker.heal(lifeSteal);
+			}
+
+			if (!(damageSource instanceof EntityDamageSource entityDamageSource && entityDamageSource.isThorns())) {
+				var reflection = DynamicModification.create()
+						.withPositive(PuffishAttributes.DAMAGE_REFLECTION, entity)
+						.relativeTo(damage);
+
+				if (reflection > 0) {
+					attacker.damage(DamageSource.thorns(entity), reflection);
+				}
 			}
 		}
 	}
