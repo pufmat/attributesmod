ca5ee0bb8729926ac002304ad198b3dfecca5e09
Pufferfish <puffish@puffish.net>
2026-01-30 13:35:19 +0100

    Add workaround in experience attribute

--- Common/src/main/java/net/puffish/attributesmod/mixin/ExperienceOrbEntityMixin.java
+++ Common/src/main/java/net/puffish/attributesmod/mixin/ExperienceOrbEntityMixin.java
@@ -1,29 +1,25 @@
 package net.puffish.attributesmod.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.puffish.attributesmod.api.DynamicModification;
 import net.puffish.attributesmod.api.PuffishAttributes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ExperienceOrbEntity.class)
 public class ExperienceOrbEntityMixin {
 
-	@ModifyArg(
-			method = "onPlayerCollision",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/entity/player/PlayerEntity;addExperience(I)V"
-			),
-			index = 0
-	)
-	private int modifyArgAtAddExperience(int experience, @Local(argsOnly = true) PlayerEntity player) {
-		return Math.round(DynamicModification.create()
+	@WrapMethod(method = "onPlayerCollision")
+	private void wrapMethodOnPlayerCollision(PlayerEntity player, Operation<Integer> original) {
+		// workaround for mods that inject and cancel
+		var previousExperience = player.totalExperience;
+		original.call(player);
+		player.addExperience(Math.round(DynamicModification.create()
 				.withPositive(PuffishAttributes.EXPERIENCE, player)
-				.applyTo(experience));
+				.relativeTo(player.totalExperience - previousExperience)
+		));
 	}
 
 }
